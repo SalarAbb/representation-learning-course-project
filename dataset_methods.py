2c68342db40e79f866fb4702e16ce057b2c0f81e
SalarAbb <habbaspo@usc.edu>
2019-12-13 18:22:16 -0800

    adds SDVAE for the dsprites dataset

--- dataset_methods.py
+++ dataset_methods.py
@@ -18,3 +18,34 @@ class data_set_model(object):
         self.dim_x = np.shape(x_test)[1]
         self.dim_y = np.shape(y_test)[1]
 
+
+class dataset_dsprites:
+    def __init__(self,options):
+        ids = np.load('ids.npz')
+        self.ids = ids
+        self.options = options
+        if options['supervise_what'] == 'shape':
+            self.index_supervision = 1
+        elif options['supervise_what'] == 'size':
+            self.index_supervision = 2
+
+    def load_dataset(self):
+        data = np.load('data/dsprites-dataset/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz') # download from github
+        print(data.files) #Prints: [’’imgs’, ’latents_classes’, ’latents_values’, ...]
+        all_imgs = data['imgs']
+        self.x_train = all_imgs[ self.ids[ 'train' ] ] # please only access imgs of ’train’
+        self.x_test = all_imgs[ self.ids['test_reconstruct'] ]
+        self.y_train = data['latents_classes'][ self.ids[ 'train' ] ][:,self.index_supervision]
+        self.y_test = data['latents_classes'][ self.ids['test_reconstruct'] ][:,self.index_supervision]
+        if self.options['supervise_what'] == 'shape':
+            self.dim_y = 3
+        elif self.options['supervise_what'] == 'size':
+            self.dim_y = 6
+        # You can access all attributes for supervised_train!
+        # self.sup_train_images = all_imgs[ self.ids[ 'supervised_train' ] ]
+        # self.sup_train_factors = data['latents_values'][ self.ids[ 'supervised_train' ] ]
+        # self.sup_train_classess = data['latents_classes'][self.ids['supervised_train'] ]
+        # self.test_reconstruct = all_imgs[ self.ids['test_reconstruct'] ] # for q1_a.pdf
+        # self.test_interpolate = all_imgs[ self.ids['test_interpolate'] ] # for q1_b.pdf
+
+
--- script_supervision_gvae.py
+++ script_supervision_gvae.py
@@ -6,7 +6,7 @@ import os
 import sys
 import numpy as np
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-from supervised_vae_methods_images import supervised_vae_model
+from supervised_vae_methods_images import supervised_gammavae_model
 from dataset_methods import data_set_model,dataset_dsprites
 
 # get the data from text dataset
@@ -16,15 +16,17 @@ from dataset_methods import data_set_model,dataset_dsprites
 
 # assign values to dataset class the 
 #dataset_text = data_set_model(x_train = points_train, x_test = points_test, options = [])
-dataset_to_train = dataset_dsprites()
+options_dataset = {}
+options_dataset['supervise_what'] = 'size'
+dataset_to_train = dataset_dsprites(options = options_dataset)
 dataset_to_train.load_dataset()
 #dataset_to_train = data_set_model(x_train = points_train, x_test = points_test, options = [])
-# create bvae options
+# create gvae options
 options = {}
 options['dim_latent'] = 5 # dimension of latent variabels should be
 options['activation_type'] = 'relu' # activation function in the layers 'tanh' or None or 'relu' or 'elu'
 options['learning_rate'] = 1e-3 # what is learning rate (usually 0.001 to 0.01)
-options['num_epochs'] = 400 # number of epochs
+options['num_epochs'] = 300 # number of epochs
 options['batch_size'] = 200 # number of batches in trainig
 options['iteration_plot_show'] = 1000 # when to plot the results
 options['print_info_step'] = 50 # how many steps to show info
@@ -33,27 +35,26 @@ options['plot_mode'] = 'save' #'save' or 'show'
 options['optimizer_type'] = 'Adam' # 'Adam' or 'GD'
 options['train_restore'] = 'restore' # 'train' or 'resotre'
 options['dropout_rate'] = 0.3
-options['save_global_directory'] = 'D:/Codes-Main-Python/cs699/Project/results'
+options['save_global_directory'] = 'D:/Codes-Main-Python/cs699/Project/results_supervised_{}'.format(options_dataset['supervise_what'])
 options['index_latent_supervised'] = np.arange(1) # this specifies the index in latent factors to be correlated with supervised target
-
-bvaemodel = supervised_vae_model(data_set = dataset_to_train, beta = 0, mu = 10, options = options)
+capacity_schedule = np.concatenate( ( np.zeros((10))  ,  np.linspace(0,25,options['num_epochs'] - 20) , 25 * np.ones((10)) )  )
+gvaemodel = supervised_gammavae_model(data_set = dataset_to_train, gamma = 5, capacity_schedule = capacity_schedule,mu = 50, options = options)
 if options['train_restore'] is 'train':
-    bvaemodel.build_computation_graph()
-    bvaemodel.train_model()
+    gvaemodel.build_computation_graph()
+    gvaemodel.train_model()
 elif options['train_restore'] is 'restore':
-    bvaemodel.load_model(epoch_num = 122)
+    gvaemodel.load_model(epoch_num = 300)
     sup_train_classes = dataset_to_train.y_train[:100]
     sup_train_images = dataset_to_train.x_train[:100,:,:]
     
-    model_factors_mu, model_factors_log_sigma2 = bvaemodel.project_to_latent(sup_train_images)
+    model_factors_mu, model_factors_log_sigma2 = gvaemodel.project_to_latent(sup_train_images)
     model_factors_mu_test = np.copy(model_factors_mu)
-    model_factors_mu_test[:,0] = model_factors_mu_test[:,0] +1
-    bvaemodel.plot_images_from_latent(sup_train_images[0:20,:,:],model_factors_mu_test[0:20,:])
+    model_factors_mu_test[:,0] = model_factors_mu_test[:,0] 
+    gvaemodel.plot_images_from_latent(sup_train_images[0:20,:,:],model_factors_mu_test[0:20,:])
+    gvaemodel.render_images_by_mixing_latent_factors(sup_train_images[7,:,:],sup_train_images[6,:,:])
     a = 1
-    # for homework plot
-    #bvaemodel.render_images_by_mixing_latent_factors(sup_train_images[7,:,:],sup_train_images[30,:,:])
-    #gvaemodel.render_images_by_mixing_latent_factors(sup_train_images[12,:,:],sup_train_images[52,:,:])
+    #gvaemodel.render_images_by_mixing_latent_factors(sup_train_images[9,:,:],sup_train_images[17,:,:])
     #gvaemodel.render_images_by_mixing_latent_factors(sup_train_images[394,:,:],sup_train_images[52,:,:])
     #gvaemodel.render_images_by_mixing_latent_factors(sup_train_images[208,:,:],sup_train_images[203,:,:])
     #gvaemodel.render_images_by_mixing_latent_factors(sup_train_images[342,:,:],sup_train_images[354,:,:])
-    #bvaemodel.render_images_by_mixing_latent_factors(sup_train_images[340,:,:],sup_train_images[314,:,:])
+    #gvaemodel.render_images_by_mixing_latent_factors(sup_train_images[340,:,:],sup_train_images[314,:,:])
